f6584a040c58f049600aa087c79c86a15e3e6ca2
dannyk-dev <kruger.dkk@gmail.com>
2024-10-11 10:43:12 -0300

    done

--- app/components/storefront/Hero.tsx
+++ app/components/storefront/Hero.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 import Slider from "./Slider";
+import Link from "next/link";
 
 async function getData() {
   const data = await prisma.banner.findMany({
@@ -19,8 +20,55 @@ async function getData() {
   return data;
 }
 
+const bgColors = [
+  "bg-gradient-to-r from-yellow-50 to-pink-50",
+  "bg-gradient-to-r from-pink-50 to-blue-50",
+  "bg-gradient-to-r from-blue-50 to-yellow-50",
+];
+
 export async function Hero() {
   const data = await getData();
 
-  return <Slider banners={data} />;
+  return (
+    <Carousel>
+      <CarouselContent className="h-screen w-full">
+        {data.map((banner, index) => (
+          <CarouselItem key={banner.id} className="w-full">
+            <div
+              className={`${
+                bgColors[index > bgColors.length - 1 ? 0 : index]
+              } w-full h-full grid lg:grid-cols-2 rounded-lg`}
+            >
+              <div className="h-1/2 w-full xl:h-full flex flex-col items-center justify-center gap-y-8 2xl:gap-y-12 text-center">
+                <h2 className="text-xl lg:text-2xl 2xl:text-5xl">
+                  {banner.description}
+                </h2>
+                <h1 className="text-5xl lg:text-5xl 2xl:text-8xl font-semibold">
+                  {banner.title}
+                </h1>
+                <Link href={banner.url || "#"}>
+                  <button className="rounded-md bg-black text-white py-3 px-4 ">
+                    SHOP NOW
+                  </button>
+                </Link>
+              </div>
+              {/* IMAGE CONTAINER */}
+              <div className="h-1/2 w-full  xl:h-full relative">
+                <Image
+                  src={banner.imageString}
+                  alt=""
+                  fill
+                  sizes="100%"
+                  className="object-cover rounded-lg"
+                />
+              </div>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="ml-16" />
+      <CarouselNext className="mr-16" />
+    </Carousel>
+  );
+  // return <Slider banners={data} />;
 }
